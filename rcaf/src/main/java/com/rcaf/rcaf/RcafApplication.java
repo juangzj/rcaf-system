9c821e904ef79c83338767dc5d91f6fe0332eb04
juanzgj <juanjosegoyeszambrano@gmail.com>
2024-12-30 14:41:49 -0500

    rcaf system startup

---
+++ rcaf/src/main/java/com/rcaf/rcaf/RcafApplication.java
@@ -0,0 +1,13 @@
+package com.rcaf.rcaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcafApplication.class, args);
+	}
+
+}
---
+++ rcaf/src/test/java/com/rcaf/rcaf/RcafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcaf.rcaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
