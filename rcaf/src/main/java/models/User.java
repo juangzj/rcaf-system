0ed58f98e36e4f733fdc8bd1da3fb63b173bef41
juanzgj <juanjosegoyeszambrano@gmail.com>
2025-01-01 11:24:57 -0500

    [added] user model added

---
+++ rcaf/src/main/java/models/User.java
@@ -0,0 +1,74 @@
+package models;
+
+import java.sql.Date;
+
+public class User {
+
+    /**
+     * User atributes
+     */
+    private int user_id;
+    private String name;
+    private String email;
+    private String password;
+    private String user_type;
+    private String identification;
+    private Date registration_date;
+
+    // Getters and setters
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUser_type() {
+        return user_type;
+    }
+
+    public void setUser_type(String user_type) {
+        this.user_type = user_type;
+    }
+
+    public String getIdentification() {
+        return identification;
+    }
+
+    public void setIdentification(String identification) {
+        this.identification = identification;
+    }
+
+    public Date getRegistration_date() {
+        return registration_date;
+    }
+
+    public void setRegistration_date(Date registration_date) {
+        this.registration_date = registration_date;
+    }
+}
