50a3793c6b1015309cd5943b76f90ae9e91854a1
juanzgj <juanjosegoyeszambrano@gmail.com>
2025-01-03 10:25:33 -0500

    [feat] use of lombok in user model

--- rcaf/src/main/java/models/User.java
+++ rcaf/src/main/java/models/User.java
@@ -1,7 +1,11 @@
 package models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.sql.Date;
 
@@ -9,71 +13,21 @@ import java.sql.Date;
 @Table(name = "Users")
 public class User {
 
-    /**
-     * User atributes
-     */
+    @Id
+    @Getter @Setter @Column( name = "user_id")
     private int user_id;
+    @Getter @Setter @Column( name = "name")
     private String name;
+    @Getter @Setter @Column( name = "email")
     private String email;
+    @Getter @Setter @Column( name = "password")
     private String password;
+    @Getter @Setter @Column( name = "user_type")
     private String user_type;
+    @Getter @Setter @Column( name = "identification")
     private String identification;
+    @Getter @Setter @Column( name = "registration_date")
     private Date registration_date;
 
-    // Getters and setters
-    public int getUser_id() {
-        return user_id;
-    }
 
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUser_type() {
-        return user_type;
-    }
-
-    public void setUser_type(String user_type) {
-        this.user_type = user_type;
-    }
-
-    public String getIdentification() {
-        return identification;
-    }
-
-    public void setIdentification(String identification) {
-        this.identification = identification;
-    }
-
-    public Date getRegistration_date() {
-        return registration_date;
-    }
-
-    public void setRegistration_date(Date registration_date) {
-        this.registration_date = registration_date;
-    }
 }
